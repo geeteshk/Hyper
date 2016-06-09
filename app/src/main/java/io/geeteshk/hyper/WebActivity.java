c1fc3acec3aaa04cb6bbb09685c7d80d8e6470d7
Geetesh Kalakoti <kalakotig@gmail.com>
2016-06-09 16:10:26 +0100

    WebActivity: Show colour and open in browser
    Commit title pretty much says it all. Now displays the project colour and allows you to open your project in a browser.

--- app/src/main/java/io/geeteshk/hyper/WebActivity.java
+++ app/src/main/java/io/geeteshk/hyper/WebActivity.java
@@ -1,20 +1,25 @@
 package io.geeteshk.hyper;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.webkit.WebView;
-import android.widget.Toast;
 
+import io.geeteshk.hyper.util.JsonUtil;
 import io.geeteshk.hyper.util.NetworkUtil;
 
 /**
  * Activity to test projects
  */
 public class WebActivity extends AppCompatActivity {
-
-    private static final String TAG = "WebSettings";
 
     /**
      * Called when the activity is created
@@ -30,16 +35,16 @@ public class WebActivity extends AppCompatActivity {
         assert toolbar != null;
         toolbar.setTitle(getIntent().getStringExtra("name"));
         setSupportActionBar(toolbar);
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor(JsonUtil.getProjectProperty(getIntent().getStringExtra("name"), "color"))));
+
 
         if (Build.VERSION.SDK_INT >= 21) {
-            getWindow().setStatusBarColor(0xFFE64A19);
+            getWindow().setStatusBarColor(Color.parseColor(JsonUtil.getProjectProperty(getIntent().getStringExtra("name"), "color")));
         }
 
         WebView webView = (WebView) findViewById(R.id.web_view);
         assert webView != null;
         webView.loadUrl(getIntent().getStringExtra("url"));
-
-        Toast.makeText(this, getIntent().getStringExtra("url"), Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -48,5 +53,24 @@ public class WebActivity extends AppCompatActivity {
         if (getIntent().getBooleanExtra("pilot", false) && NetworkUtil.getDrive() != null) {
             NetworkUtil.getDrive().stop();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_web, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.web_browser:
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(getIntent().getStringExtra("url")));
+                startActivity(intent);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
