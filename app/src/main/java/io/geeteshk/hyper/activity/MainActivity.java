9b6b3c19172c0913ff974839cb7ac7818dfaa95c
Geetesh Kalakoti <kalakotig@gmail.com>
2016-11-15 11:53:26 -0600

    Switch to Apache 2.0

--- app/src/main/java/io/geeteshk/hyper/activity/MainActivity.java
+++ app/src/main/java/io/geeteshk/hyper/activity/MainActivity.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2016 Geetesh Kalakoti <kalakotig@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.geeteshk.hyper.activity;
 
 import android.annotation.SuppressLint;
@@ -59,6 +75,7 @@ import java.util.Iterator;
 import java.util.Locale;
 
 import de.psdev.licensesdialog.LicensesDialog;
+import de.psdev.licensesdialog.licenses.ApacheSoftwareLicense20;
 import de.psdev.licensesdialog.licenses.License;
 import de.psdev.licensesdialog.licenses.MITLicense;
 import de.psdev.licensesdialog.model.Notice;
@@ -363,7 +380,7 @@ public class MainActivity extends AppCompatActivity {
                 final String name = "Hyper";
                 final String url = "http://geeteshk.github.io/Hyper";
                 final String copyright = "Copyright (c) 2016 Geetesh Kalakoti <kalakotig@gmail.com>";
-                final License license = new MITLicense();
+                final License license = new ApacheSoftwareLicense20();
                 final Notice notice = new Notice(name, url, copyright, license);
                 new LicensesDialog.Builder(this)
                         .setNotices(notice)
