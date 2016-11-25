aee4e0561c0046b27b42d6803a844aa534c0c79f
Geetesh Kalakoti <kalakotig@gmail.com>
2016-11-25 11:48:25 -0600

    Added Factory Reset option

--- app/src/main/java/io/geeteshk/hyper/fragment/SettingsFragment.java
+++ app/src/main/java/io/geeteshk/hyper/fragment/SettingsFragment.java
@@ -1,13 +1,23 @@
 package io.geeteshk.hyper.fragment;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.preference.SwitchPreference;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.pavelsikun.seekbarpreference.SeekBarPreference;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
 
 import de.psdev.licensesdialog.LicensesDialog;
 import de.psdev.licensesdialog.licenses.ApacheSoftwareLicense20;
@@ -15,10 +25,13 @@ import de.psdev.licensesdialog.licenses.License;
 import de.psdev.licensesdialog.model.Notice;
 import io.geeteshk.hyper.R;
 import io.geeteshk.hyper.activity.SettingsActivity;
+import io.geeteshk.hyper.helper.Constants;
 import io.geeteshk.hyper.helper.Pref;
 import io.geeteshk.hyper.helper.Theme;
 
 public class SettingsFragment extends PreferenceFragment {
+
+    private static final String TAG = SettingsFragment.class.getSimpleName();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -28,7 +41,7 @@ public class SettingsFragment extends PreferenceFragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        SwitchPreference darkTheme = (SwitchPreference) getPreferenceManager().findPreference("dark_theme");
+        final SwitchPreference darkTheme = (SwitchPreference) getPreferenceManager().findPreference("dark_theme");
         darkTheme.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
             @Override
             public boolean onPreferenceChange(Preference preference, Object o) {
@@ -40,7 +53,7 @@ public class SettingsFragment extends PreferenceFragment {
             }
         });
 
-        SwitchPreference darkThemeEditor = (SwitchPreference) getPreferenceManager().findPreference("dark_theme_editor");
+        final SwitchPreference darkThemeEditor = (SwitchPreference) getPreferenceManager().findPreference("dark_theme_editor");
         darkThemeEditor.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
             @Override
             public boolean onPreferenceChange(Preference preference, Object o) {
@@ -49,8 +62,34 @@ public class SettingsFragment extends PreferenceFragment {
             }
         });
 
-        Preference preference = getPreferenceManager().findPreference("notices");
-        preference.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+        final Preference factoryReset = getPreferenceManager().findPreference("factory_reset");
+        factoryReset.setEnabled(new File(Constants.HYPER_ROOT).list().length > 0);
+        factoryReset.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                AlertDialog.Builder resetBuilder = new AlertDialog.Builder(getActivity());
+                resetBuilder.setTitle("Factory Reset");
+                resetBuilder.setMessage("Are you sure you want to delete ALL of your projects? This change cannot be undone!");
+                resetBuilder.setPositiveButton("RESET", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        try {
+                            FileUtils.cleanDirectory(new File(Constants.HYPER_ROOT));
+                            factoryReset.setEnabled(false);
+                        } catch (IOException e) {
+                            Log.e(TAG, e.toString());
+                        }
+                    }
+                });
+
+                resetBuilder.setNegativeButton("CANCEL", null);
+                resetBuilder.create().show();
+                return true;
+            }
+        });
+
+        Preference notices  = getPreferenceManager().findPreference("notices");
+        notices.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
             @Override
             public boolean onPreferenceClick(Preference preference) {
                 new LicensesDialog.Builder(getActivity())
