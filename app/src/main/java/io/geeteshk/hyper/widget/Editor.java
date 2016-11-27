4a447b2353089d734d016648ed1ae16eb2fe50b1
Geetesh Kalakoti <kalakotig@gmail.com>
2016-11-27 11:08:00 -0600

    Return better token after autocompletion

--- app/src/main/java/io/geeteshk/hyper/widget/Editor.java
+++ app/src/main/java/io/geeteshk/hyper/widget/Editor.java
@@ -557,7 +557,7 @@ public class Editor extends MultiAutoCompleteTextView {
                 int len = text.length();
 
                 while (i < len) {
-                    if (text.charAt(i) == ' ') {
+                    if (text.charAt(i) == ' ' || text.charAt(i) == '>' || text.charAt(i) == '/') {
                         return i;
                     } else {
                         i++;
@@ -579,11 +579,11 @@ public class Editor extends MultiAutoCompleteTextView {
                     return text;
                 } else {
                     if (text instanceof Spanned) {
-                        SpannableString sp = new SpannableString(text + " ");
+                        SpannableString sp = new SpannableString(text);
                         TextUtils.copySpansFrom((Spanned) text, 0, text.length(), Object.class, sp, 0);
                         return sp;
                     } else {
-                        return text + " ";
+                        return text + "></" + text + ">";
                     }
                 }
             }
