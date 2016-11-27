995ff2f0d2426430501a070667474fdf763f0cc8
Geetesh Kalakoti <kalakotig@gmail.com>
2016-11-26 18:53:32 -0600

    Added viewport tag

--- app/src/main/java/io/geeteshk/hyper/helper/Project.java
+++ app/src/main/java/io/geeteshk/hyper/helper/Project.java
@@ -47,11 +47,12 @@ public class Project {
     /**
      * HTML BareBones Template
      */
-    public static final String INDEX = "<!doctype html>\n" +
+    private static final String INDEX = "<!doctype html>\n" +
             "<html>\n" +
             "  <head>\n" +
             "    <meta charset=\"UTF-8\">\n" +
             "    <title>@name</title>\n" +
+            "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n" +
             "    <meta name=\"author\" content=\"@author\">\n" +
             "    <meta name=\"description\" content=\"@description\">\n" +
             "    <meta name=\"keywords\" content=\"@keywords\">\n" +
@@ -67,12 +68,12 @@ public class Project {
     /**
      * Empty Style
      */
-    public static final String STYLE = "/* Add all your styles here */";
+    private static final String STYLE = "/* Add all your styles here */";
 
     /**
      * Empty Script
      */
-    public static final String MAIN = "// Add all your JS here";
+    private static final String MAIN = "// Add all your JS here";
 
     /**
      * Log TAG
