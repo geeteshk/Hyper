42b2ebd10c290afe5d309ef691e745af63aeb6b8
Geetesh Kalakoti <kalakotig@gmail.com>
2016-12-16 14:58:20 -0600

    Fix incorrect path being used for displaying images

--- app/src/main/java/io/geeteshk/hyper/fragment/ImageFragment.java
+++ app/src/main/java/io/geeteshk/hyper/fragment/ImageFragment.java
@@ -22,6 +22,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -54,7 +55,9 @@ public class ImageFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        File file = new File(Constants.HYPER_ROOT + File.separator + getArguments().getString("location"));
+        String location = getArguments().getString("location");
+        assert location != null;
+        File file = new File(location);
         if (!file.exists()) {
             TextView textView = new TextView(getActivity());
             textView.setText(R.string.file_problem);
